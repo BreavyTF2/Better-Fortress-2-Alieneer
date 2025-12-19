d24731cff50e54851420a5d246aa223e3d56a8eb
TheGabenZone <jmonteiro2002.77@gmail.com>
2025-12-19 09:04:54 +0000

    Updated build.yml

--- src/game/server/gameinterface.cpp
+++ src/game/server/gameinterface.cpp
@@ -331,6 +331,7 @@ int UTIL_PlayerIsModDev( CBasePlayer *client )
 		//Contributors
 		case 76561198813329543: // Grub - it's grubbin time.
 		case 76561198024846297: // SaintSoftware
+		case 76561198423023261: // Alieneer/MOTS0
 			return 3;
 		break;
 		//None
